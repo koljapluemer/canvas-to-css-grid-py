f6ff28b5fc5bb99f442f8becf92df5b45ca777d8
koljapluemer <github@koljapluemer.com>
2025-05-22 17:59:34 +0200

    green test

--- src/classes/object_manager.py
+++ src/classes/object_manager.py
@@ -3,8 +3,11 @@ class ObjectManager:
     edges = []
 
     @staticmethod
-    def create_from_JSON(self, file):
-        return 
+    def create_from_JSON(json_data):
+        obj_manager = ObjectManager()
+        obj_manager.nodes = json_data.get('nodes', [])
+        obj_manager.edges = json_data.get('edges', [])
+        return obj_manager
     
     def export_to_JSON(self):
         return {}
